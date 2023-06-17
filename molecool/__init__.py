c5a9596a3128c77d465f020c5d18c68ad3f4587f
Norman_Tran <nttran@andrew.cmu.edu>
2023-06-16 21:50:43 -0400

    split the function into subpackages

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -3,6 +3,13 @@ This was created for the Python Best Practice Workshop"""
 
 # Add imports here
 from .functions import canvas
+from .measure import calculate_angle
+from molecool.atom_data import atom_colors,atomic_weights
 
+from molecool.visualization import draw_molecule
+
+from molecool.molecules import bond_histogram, build_bond_list
+
+from molecool.io import pdb, xyz
 
 from ._version import __version__
